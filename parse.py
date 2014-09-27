471de1bfaefb05e5da6dddce3576e0318ba2d0bb
Andrey <nezaidu@gmail.com>
2014-09-28 03:07:52 +0400

    added multithreading.works like a charm!!!

--- parse.py
+++ parse.py
@@ -5,29 +5,33 @@ from get_translation import get
 from find import find
 from detp import fun
 from clean import clean
-words=[line[0:-1].lower() for line in open(sys.argv[3]+'.txt','r')]
+from threading import Thread
+from Queue import Queue
+words=[line[0:-1].lower() for line in open('dictionary.txt','r')]
 words.sort()
 subs = pysrt.open('final.srt',encoding='utf-8')
 done=[]
-arg1=int(sys.argv[1])
-arg2=int(sys.argv[2])
-for i in range(arg1 or 0,arg2 or len(subs)):
-	print i,100*(float(i)/len(subs))
-	sen=subs[i].text
+totranslate=[]
+translated=[]
+count=0
+def parse(index):
+	global count
+	global totranslate
+	# print index,100*(float(index)/len(subs))
+	sen=subs[index].text
 	k=[]
 	k2={}
 	par=fun(sen)
 	dic={}
-	#print par
 	for word in ' '.join(sen.split('\n')).split(' '):
-		# print word,find(words,clean(word))
 		if find(words,clean(word))==0:
-		    k+=[clean(word)]
+			k+=[clean(word)]
 	for key in par:
 		k2[clean(key)]=par[key]
-	# print par
 	for key in k:
-		dic[key]=get(key,k2[key])
+		dic[key]='{'+str(count)+'}'
+		totranslate+=[(key,k2[key])]
+		count+=1
 	l=sen.split('\n')
 	sen=' '.join(l)
 	l=sen.split(' ')
@@ -36,8 +40,36 @@ for i in range(arg1 or 0,arg2 or len(subs)):
 		if word in dic:
 			l[i]+='<font color="#ff0000">'+dic[word]+'</font>'
 	sen=' '.join(l)
-	done.append(sen)
-for i in range(len(done)):
-	subs[i].text=done[i]
+	subs[index].text=sen
+for i in range(len(subs)):
+	print i
+	parse(i)
 subs.save('duck2.srt', encoding='utf-8')
+def worker():
+	global totranslate
+	global translated
+	while True:
+		index = q.get()
+		print 'working with '+str(index)
+		totranslate[index]=get(totranslate[index][0],totranslate[index][1])
+		q.task_done()
+
+q = Queue()
+for i in range(30):
+	 t = Thread(target=worker)
+	 t.daemon = True
+	 t.start()
+
+for index in range(count):
+	q.put(index)
+
+q.join()   
+# print totranslate
+# print translated
+import codecs
+f=codecs.open('duck2.srt',mode='r+',encoding='utf-8')
+s=f.read()
+f.close()
+f=codecs.open('duck5.srt',mode='w+',encoding='utf-8')
+f.write(s.format(*totranslate))
 f.close()
