18a63dcadcc7aa8ee785acf0d63d5736f264cc45
Andrey <nezaidu@gmail.com>
2014-09-28 19:42:11 +0400

    fully added threads

--- parse.py
+++ parse.py
@@ -1,75 +1,56 @@
-# coding: utf-8
 import sys
 import pysrt
-from get_translation import get
-from find import find
-from detp import fun
-from clean import clean
 from threading import Thread
 from Queue import Queue
-words=[line[0:-1].lower() for line in open('dictionary.txt','r')]
-words.sort()
-subs = pysrt.open('final.srt',encoding='utf-8')
+import argparse
+import codecs
+from shutil import copyfile
+from parse_line import *
 done=[]
 totranslate=[]
-translated=[]
-count=0
-def parse(index):
-	global count
+def parse_file(f):
 	global totranslate
-	# print index,100*(float(index)/len(subs))
-	sen=subs[index].text
-	k=[]
-	k2={}
-	par=fun(sen)
-	dic={}
-	for word in ' '.join(sen.split('\n')).split(' '):
-		if find(words,clean(word))==0:
-			k+=[clean(word)]
-	for key in par:
-		k2[clean(key)]=par[key]
-	for key in k:
-		dic[key]='{'+str(count)+'}'
-		totranslate+=[(key,k2[key])]
-		count+=1
-	l=sen.split('\n')
-	sen=' '.join(l)
-	l=sen.split(' ')
-	for i in range(len(l)):
-		word=clean(l[i])
-		if word in dic:
-			l[i]+='<font color="#ff0000">'+dic[word]+'</font>'
-	sen=' '.join(l)
-	subs[index].text=sen
-for i in range(len(subs)):
-	print i
-	parse(i)
-subs.save('duck2.srt', encoding='utf-8')
-def worker():
-	global totranslate
-	global translated
-	while True:
-		index = q.get()
-		print 'working with '+str(index)
-		totranslate[index]=get(totranslate[index][0],totranslate[index][1])
-		q.task_done()
+	f+='.srt'
+	subs = pysrt.open(f,encoding='utf-8')
+	copyfile(f,'OLD'+f)
+	for i in range(len(subs)):
+		print i
+		sen=subs[i].text
+		parsed_line=parse_line(sen,len(totranslate))
+		subs[i].text=parsed_line[0]
+		totranslate+=parsed_line[1]
+	subs.save(f, encoding='utf-8')
+	def worker():
+		global totranslate
+		global translated
+		while True:
+			index = q.get()
+			print 'working with '+str(index)
+			totranslate[index]=get(totranslate[index][0],totranslate[index][1])
+			q.task_done()
 
-q = Queue()
-for i in range(30):
-	 t = Thread(target=worker)
-	 t.daemon = True
-	 t.start()
+	q = Queue()
+	for i in range(30):
+		 t = Thread(target=worker)
+		 t.daemon = True
+		 t.start()
 
-for index in range(count):
-	q.put(index)
+	for index in range(len(totranslate)):
+		q.put(index)
 
-q.join()   
-# print totranslate
-# print translated
-import codecs
-f=codecs.open('duck2.srt',mode='r+',encoding='utf-8')
-s=f.read()
-f.close()
-f=codecs.open('duck5.srt',mode='w+',encoding='utf-8')
-f.write(s.format(*totranslate))
-f.close()
+	q.join()   
+	fl=codecs.open(f,mode='r+',encoding='utf-8')
+	s=fl.read()
+	fl.close()
+	fl=codecs.open(f,mode='w+',encoding='utf-8')
+	fl.truncate()
+	fl.write(s.format(*totranslate))
+	fl.close()
+#dispensed
+
+parser = argparse.ArgumentParser()
+parser.add_argument('mode', choices=['file','dir'],help='You can parse file or entire directory.')
+parser.add_argument('destination',help='Destination to file/directory.')
+args = parser.parse_args()
+if args.mode=='file':
+	parse_file(args.destination)
---
+++ parse_line.py
@@ -0,0 +1,33 @@
+from get_translation import get
+from find import find
+from detp import fun
+from clean import clean
+words=[line[0:-1].lower() for line in open('dictionary.txt','r')]
+words.sort()
+def parse_line(sen,count):
+	totranslate=[]
+	k=[]
+	k2={}
+	dic={}
+	for word in ' '.join(sen.split('\n')).split(' '):
+		if find(words,clean(word))==0:
+			k+=[clean(word)]
+	if len(k)==0:
+		return [sen,[]]
+	else:
+		par=fun(sen)
+	for key in par:
+		k2[clean(key)]=par[key]
+	for key in k:
+		dic[key]='{'+str(count)+'}'
+		totranslate+=[(key,k2[key])]
+		count+=1
+	l=sen.split('\n')
+	sen=' '.join(l)
+	l=sen.split(' ')
+	for i in range(len(l)):
+		word=clean(l[i])
+		if word in dic:
+			l[i]+='<font color="#ff0000">'+dic[word]+'</font>'
+	sen=' '.join(l)
+	return [sen,totranslate]
