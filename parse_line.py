2df5d924aefa6b7c888b369eeb0c7a23f7dafa1c
Andrey <nezaidu@gmail.com>
2015-01-18 19:25:22 +0300

    nltk converts "wanna" in want to.patched this

--- parse_line.py
+++ parse_line.py
@@ -1,9 +1,13 @@
 from get_translation import get
 from find import find
-from detp import fun
+from detect_part import detect
 from clean import clean
+
+
 words=[line[0:-1].lower() for line in open('dictionary.txt','r')]
+# words=['wanna','gonna']
 words.sort()
+
 def parse_line(sen,count):
 	totranslate=[]
 	k=[]
@@ -15,13 +19,14 @@ def parse_line(sen,count):
 	if len(k)==0:
 		return [sen,[]]
 	else:
-		par=fun(sen)
+		par=detect(sen)
 	for key in par:
 		k2[clean(key)]=par[key]
 	for key in k:
-		dic[key]='{'+str(count)+'}'
-		totranslate+=[(key,k2[key])]
-		count+=1
+		if key in k2:
+			dic[key]='{'+str(count)+'}'
+			totranslate+=[(key,k2[key])]
+			count+=1
 	l=sen.split('\n')
 	sen=' '.join(l)
 	l=sen.split(' ')
