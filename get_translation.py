e5809fc280314a54fcc8d70b271bcc86e7e9c29a
Vlad Kostin <vlad.kostin@gmail.com>
2015-04-11 13:35:05 +0300

    fixed trouble with google translate.i have no idea for how long it will be working now.

--- get_translation.py
+++ get_translation.py
@@ -1,49 +1,56 @@
 import urllib2
-from pattern.en import lemma,singularize
+from pattern.en import lemma, singularize
 
-def get(word,type,number_of_translations=4):
-	types=type.split(',')
-	if len(types)!=1:
-		return get(word,types[0],2)+'||'+get(word,types[1],2)
-	try:
-		word=word.encode('ascii')
-	except UnicodeDecodeError:
-		return ''
-	opener = urllib2.build_opener()
-	opener.addheaders = [('User-agent', 'Mozilla/5.0')]
-	res = opener.open('http://translate.google.com/translate_a/t?client=t&sl=en&tl=ru&hl=en&sc=2&ie=UTF-8&oe=UTF-8&oc=1&otf=1&ssel=0&tsel=0&q='+word).read()
-	tran={}
-	every_part=''
-	res=res.replace('adverb','adver_')
-	parts_of_speech=['adver_','verb','noun','conjunction','preposition','adjective']
-	for part in parts_of_speech:
-		if res.find(part)!=-1:
-			index=res.find(part)+len(part)+4
-			# print res[res.find(part):res.find(part)+len(part)]
-			verge=res[index:].find(']')+len(res[0:index])-1
-			tran[part]=(res[index:verge].split('","'))[0:number_of_translations]	
-			every_part+='||'+tran[part][0]
-			tran[part]='||'.join(tran[part])
-		else:
-			tran[part]=''
-	# print str(tran['verb']).decode('utf-8')
-	if type[0]=='J':
-		return (tran['adjective'] or every_part).decode('utf-8')
-	elif type[0]=='V':
-			if type!='VB':
-				return get(lemma(word),'VB')
-			return (tran['verb'] or every_part).decode('utf-8')
-	elif type[0]=='N':
-		if type=='NNP':
-			t=get(word,'NN')
-			if t:
-				return t
-		if type=='NNS':
-			return(get(singularize(word),'NN') or get(word,'NN'))
-		return	(tran['noun'] or every_part).decode('utf-8')
-	elif type[0]=='R':
-		return	(tran['adver_'] or every_part).decode('utf-8')
-	elif type=='CC':
-		return	(tran['conjunction'] or every_part).decode('utf-8')
-	else:
-		return every_part.decode('utf-8')
+
+def get(word, type, number_of_translations=4):
+    types = type.split(',')
+    if len(types) != 1:
+        return get(word, types[0], 2) + '||' + get(word, types[1], 2)
+    try:
+        word = word.encode('ascii')
+    except UnicodeDecodeError:
+        return ''
+    opener = urllib2.build_opener()
+    opener.addheaders = [
+    	('User-agent', 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.101 Safari/537.36'),
+    	('referer', 'https://translate.google.com/'),
+    ]
+    res = opener.open(
+        'https://translate.google.com/translate_a/single?client=t&sl=en&tl=ru&hl=ru&dt=bd&dt=ex&dt=ld&dt=md&dt=qca&dt=rw&dt=rm&dt=ss&dt=t&dt=at&ie=UTF-8&oe=UTF-8&otf=1&ssel=0&tsel=0&tk=519178|804751&q=' + word).read()
+    tran = {}
+    every_part = ''
+    res = res.replace('adverb', 'adver_')
+    parts_of_speech = [
+        'adver_', 'verb', 'noun', 'conjunction', 'preposition', 'adjective']
+    for part in parts_of_speech:
+        if res.find(part) != -1:
+            index = res.find(part) + len(part) + 4
+            # print res[res.find(part):res.find(part)+len(part)]
+            verge = res[index:].find(']') + len(res[0:index]) - 1
+            tran[part] = (res[index:verge].split('","'))[
+                0:number_of_translations]
+            every_part += '||' + tran[part][0]
+            tran[part] = '||'.join(tran[part])
+        else:
+            tran[part] = ''
+    # print str(tran['verb']).decode('utf-8')
+    if type[0] == 'J':
+        return (tran['adjective'] or every_part).decode('utf-8')
+    elif type[0] == 'V':
+        if type != 'VB':
+            return get(lemma(word), 'VB')
+        return (tran['verb'] or every_part).decode('utf-8')
+    elif type[0] == 'N':
+        if type == 'NNP':
+            t = get(word, 'NN')
+            if t:
+                return t
+        if type == 'NNS':
+            return(get(singularize(word), 'NN') or get(word, 'NN'))
+        return (tran['noun'] or every_part).decode('utf-8')
+    elif type[0] == 'R':
+        return (tran['adver_'] or every_part).decode('utf-8')
+    elif type == 'CC':
+        return (tran['conjunction'] or every_part).decode('utf-8')
+    else:
+        return every_part.decode('utf-8')
--- parse.py
+++ parse.py
@@ -8,57 +8,65 @@ import codecs
 import os
 from shutil import copyfile
 from parse_line import *
-done=[]
-totranslate=[]
+
+
+done = []
+totranslate = []
+
+
 def parse_file(f):
-	print 'processing '+f+'...'
-	global totranslate
-	copyfile(f,f.split('.srt')[0]+'OLD.srt')
-	subs = pysrt.open(f,encoding='utf-8')
-	for i in range(len(subs)):
-		print i
-		sen=subs[i].text
-		parsed_line=parse_line(sen,len(totranslate))
-		subs[i].text=parsed_line[0]
-		totranslate+=parsed_line[1]
-	subs.save(f, encoding='utf-8')
-	def worker():
-		global totranslate
-		global translated
-		while True:
-			index = q.get()
-			print 'working with '+str(index)
-			totranslate[index]=get(totranslate[index][0],totranslate[index][1])
-			q.task_done()
+    print 'processing ' + f + '...'
+    global totranslate
+    copyfile(f, f.split('.srt')[0] + 'OLD.srt')
+    subs = pysrt.open(f, encoding='utf-8')
+    for i in range(len(subs)):
+        print i
+        sen = subs[i].text
+        parsed_line = parse_line(sen, len(totranslate))
+        subs[i].text = parsed_line[0]
+        totranslate += parsed_line[1]
+    subs.save(f, encoding='utf-8')
 
-	q = Queue()
-	for i in range(30):
-		 t = Thread(target=worker)
-		 t.daemon = True
-		 t.start()
+    def worker():
+        global totranslate
+        global translated
+        while True:
+            index = q.get()
+            print 'working with ' + str(index)
+            totranslate[index] = get(
+                totranslate[index][0], totranslate[index][1])
+            q.task_done()
 
-	for index in range(len(totranslate)):
-		q.put(index)
+    q = Queue()
+    for i in range(30):
+        t = Thread(target=worker)
+        t.daemon = True
+        t.start()
 
-	q.join()   
-	fl=codecs.open(f,mode='r+',encoding='utf-8')
-	s=fl.read()
-	fl.close()
-	fl=codecs.open(f,mode='w+',encoding='utf-8')
-	fl.truncate()
-	fl.write(s.format(*totranslate))
-	fl.close()
-#dispensed
-#shed
-#margate
+    for index in range(len(totranslate)):
+        q.put(index)
+
+    q.join()
+    fl = codecs.open(f, mode='r+', encoding='utf-8')
+    s = fl.read()
+    fl.close()
+    fl = codecs.open(f, mode='w+', encoding='utf-8')
+    fl.truncate()
+    fl.write(s.format(*totranslate))
+    fl.close()
+# dispensed
+# shed
+# margate
 parser = argparse.ArgumentParser()
-parser.add_argument('mode', choices=['file','dir'],help='You can parse file or entire directory.')
-parser.add_argument('destination',help='Destination to file/directory.',nargs='+')
+parser.add_argument(
+    'mode', choices=['file', 'dir'], help='You can parse file or entire directory.')
+parser.add_argument(
+    'destination', help='Destination to file/directory.', nargs='+')
 args = parser.parse_args()
-if args.mode=='file':
-	dest=' '.join(args.destination)
-	parse_file(dest)
+if args.mode == 'file':
+    dest = ' '.join(args.destination)
+    parse_file(dest)
 else:
-	dirr=' '.join(args.destination)
-	for f in os.listdir(dirr):
-		parse_file(dirr+f)
+    dirr = ' '.join(args.destination)
+    for f in os.listdir(dirr):
+        parse_file(dirr + f)
